69691ae73b67129f7f5acf1445985b5bd001b739
Mar204 <146201644+Mar204@users.noreply.github.com>
2023-10-06 19:53:03 -0300

    Upload File Direccion.java
    Subida archivo "Direccion.java".

---
+++ src/Direccion.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+import java.util.ArrayList;
+//-------------Import todos-----------------//
+
+public class Direccion {
+    private String direccion;
+
+    public Direccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
