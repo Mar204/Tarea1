a760dc02bba5a6dff1a8e59bae01f9fa6a9a32ec
Mar204 <146201644+Mar204@users.noreply.github.com>
2023-10-06 19:51:30 -0300

    Upload File "Cliente.kava"
    Subida archivo "Cliente.java".

---
+++ src/Cliente.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+import java.util.ArrayList;
+//-------------Import todos-----------------//
+
+public class Cliente {
+    private String nombre;
+    private String rut;
+
+    public Cliente(String nombre, String rut) {
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+//-------------------------Get 'n Set----------------------------------//
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+}
