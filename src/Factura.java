36d571e5736679e26141bca4bc1ad90bd9736412
Mar204 <146201644+Mar204@users.noreply.github.com>
2023-10-06 19:55:51 -0300

    Upload File Factura.java
    Subida archivo "Factura.java".

---
+++ src/Factura.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+import java.util.ArrayList;
+//-------------Import todos-----------------//
+
+public class Factura extends DocTributario {
+    public Factura (String numero, String rut, Date fecha){
+        super (numero, rut, fecha);
+    }
+    //rellenar :)
+}
