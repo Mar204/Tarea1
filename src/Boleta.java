c923572bc33065521b9629bd6d98667ac3f9b737
Mar204 <146201644+Mar204@users.noreply.github.com>
2023-10-06 19:50:47 -0300

    Upload File Boleta.java
    Subida archivo "Boleta.java".

---
+++ src/Boleta.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+import java.util.ArrayList;
+//-------------Import todos-----------------//
+
+public class Boleta extends DocTributario {
+    public Boleta(String numero, String rut, Date fecha) {
+        super(numero, rut, fecha);
+    }
+}
